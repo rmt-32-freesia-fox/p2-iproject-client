b86821ab429f840b2fa5ae35a066b635e999f6f8
mashayyik <mashayyik@pop-os.localdomain>
2023-02-07 10:27:01 +0700

    CREATE TEMPLATE 1. feat: add Homepage template 2. feat: add LoginPage template 3. feat: add Dashboard template

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,20 @@
-<script setup>
+<script>
+import Navbar from "./components/Navbar.vue";
 import { RouterLink, RouterView } from "vue-router";
+export default {
+  components: {
+    Navbar,
+  },
+};
 </script>
 
 <template>
-  <div class="text-3xl h-screen flex justify-center items-center">
-    <div>Hello World</div>
+  <div class="bg-gray-300">
+    <!-- ====== Navbar Section Start -->
+    <Navbar />
+    <!-- ====== Navbar Section End -->
+    <div class="pt-[30px]">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
