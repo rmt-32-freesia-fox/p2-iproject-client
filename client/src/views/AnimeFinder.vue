3b53572dea83c04b143e063229ec9969c9843d17
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-02-08 05:51:42 +0700

    feat : add animefinder page

--- client/src/views/AnimeFinder.vue
+++ client/src/views/AnimeFinder.vue
@@ -1,71 +1,162 @@
 <script >
+import { mapState, mapActions } from 'pinia'
+import { useAnimeStore } from '../stores/anime'
+export default {
+  data() {
+    return {
+      inputSearch: '',
+      imageDetail: 'https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260',
+      titleDetail: 'a',
+      synopsisDetail: 'a',
+      episodesDetail: 'a',
+    }
+  },
+  methods: {
+    ...mapActions(useAnimeStore, ['searchAnime']),
+    scrollToTop() {
+    window.scrollTo(0,0);
+  }
+  },
+  computed: {
+    ...mapState(useAnimeStore, ['animeSearchResult']),
+  }
+}
+
+
+
 </script>
 
 <template>
-<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-4">
     <div class="flex flex-col lg:flex-row">
-      <div class="max-w-xl pr-16 mx-auto mb-10">
-        <h5 class="mb-6 text-3xl font-extrabold leading-none">
-          The quick, brown fox jumps over a lazy dog
-        </h5>
-        <p class="mb-6 text-gray-900">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae. Sed ut unde omnis iste natus.
-        </p>
-        <div class="flex items-center">
-          <button
-            type="submit"
-            class="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-          >
-            Get started
-          </button>
-          <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+      <div class="w-1/3  mx-auto ">
+        <div class="bg-slate-700  py-4 px-4 pt-16 mb-4 w-full">
+          <form @submit.prevent="searchAnime(inputSearch)">
+            <div class="max-w-xl">
+
+              <div class="flex space-x-4">
+                <div class="flex rounded-md overflow-hidden w-full">
+                  <input v-model="inputSearch" type="text" class="w-full rounded-md rounded-r-none" />
+                  <button type="submit"
+                    class="bg-indigo-600 text-white px-6 text-lg font-semibold py-1 rounded-r-md">Go</button>
+                </div>
+                <button @click.prevent="inputSearch = ''"
+                  class="bg-white px-2 text-sm font-semibold py-1 rounded-md">Clear</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div>
+          <img class="object-cover w-full  rounded shadow-lg "
+            :src="imageDetail"
+            alt="" />
+        </div>
+        <div class="px-4 mt-2 w-full">
+          <div class=" gap-12 row-gap-8 lg:grid-cols-2">
+            <div class="flex flex-col justify-center">
+              <div class="max-w-xl mb-1">
+                <h2
+                  class="max-w-lg mb-1 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                 {{titleDetail}}<br class="hidden md:block" />
+                </h2>
+                <div class="mt-3 flex gap-5 items-center justify-center">
+                  <div class="flex items-center justify-center gap-4">
+                    <div class="flex items-center justify-center w-5 h-5  rounded-full bg-indigo-50">
+                      <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+  <path clip-rule="evenodd" fill-rule="evenodd" d="M1 4.75C1 3.784 1.784 3 2.75 3h14.5c.966 0 1.75.784 1.75 1.75v10.515a1.75 1.75 0 01-1.75 1.75h-1.5c-.078 0-.155-.005-.23-.015H4.48c-.075.01-.152.015-.23.015h-1.5A1.75 1.75 0 011 15.265V4.75zm16.5 7.385V11.01a.25.25 0 00-.25-.25h-1.5a.25.25 0 00-.25.25v1.125c0 .138.112.25.25.25h1.5a.25.25 0 00.25-.25zm0 2.005a.25.25 0 00-.25-.25h-1.5a.25.25 0 00-.25.25v1.125c0 .108.069.2.165.235h1.585a.25.25 0 00.25-.25v-1.11zm-15 1.11v-1.11a.25.25 0 01.25-.25h1.5a.25.25 0 01.25.25v1.125a.25.25 0 01-.164.235H2.75a.25.25 0 01-.25-.25zm2-4.24v1.125a.25.25 0 01-.25.25h-1.5a.25.25 0 01-.25-.25V11.01a.25.25 0 01.25-.25h1.5a.25.25 0 01.25.25zm13-2.005V7.88a.25.25 0 00-.25-.25h-1.5a.25.25 0 00-.25.25v1.125c0 .138.112.25.25.25h1.5a.25.25 0 00.25-.25zM4.25 7.63a.25.25 0 01.25.25v1.125a.25.25 0 01-.25.25h-1.5a.25.25 0 01-.25-.25V7.88a.25.25 0 01.25-.25h1.5zm0-3.13a.25.25 0 01.25.25v1.125a.25.25 0 01-.25.25h-1.5a.25.25 0 01-.25-.25V4.75a.25.25 0 01.25-.25h1.5zm11.5 1.625a.25.25 0 01-.25-.25V4.75a.25.25 0 01.25-.25h1.5a.25.25 0 01.25.25v1.125a.25.25 0 01-.25.25h-1.5zm-9 3.125a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z"></path>
+</svg>
+                    </div>
+                    <h6 class=" font-semibold leading-5">
+                      Episodes
+                    </h6>
+                    <p class="text-sm text-gray-900">
+                      {{episodesDetail}}
+                    </p>
+                  </div>
+                  <div class="flex items-center justify-center gap-4">
+                    <div class="flex items-center justify-center w-5 h-5  rounded-full bg-indigo-50">
+                      <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+  <path clip-rule="evenodd" fill-rule="evenodd" d="M1 4.75C1 3.784 1.784 3 2.75 3h14.5c.966 0 1.75.784 1.75 1.75v10.515a1.75 1.75 0 01-1.75 1.75h-1.5c-.078 0-.155-.005-.23-.015H4.48c-.075.01-.152.015-.23.015h-1.5A1.75 1.75 0 011 15.265V4.75zm16.5 7.385V11.01a.25.25 0 00-.25-.25h-1.5a.25.25 0 00-.25.25v1.125c0 .138.112.25.25.25h1.5a.25.25 0 00.25-.25zm0 2.005a.25.25 0 00-.25-.25h-1.5a.25.25 0 00-.25.25v1.125c0 .108.069.2.165.235h1.585a.25.25 0 00.25-.25v-1.11zm-15 1.11v-1.11a.25.25 0 01.25-.25h1.5a.25.25 0 01.25.25v1.125a.25.25 0 01-.164.235H2.75a.25.25 0 01-.25-.25zm2-4.24v1.125a.25.25 0 01-.25.25h-1.5a.25.25 0 01-.25-.25V11.01a.25.25 0 01.25-.25h1.5a.25.25 0 01.25.25zm13-2.005V7.88a.25.25 0 00-.25-.25h-1.5a.25.25 0 00-.25.25v1.125c0 .138.112.25.25.25h1.5a.25.25 0 00.25-.25zM4.25 7.63a.25.25 0 01.25.25v1.125a.25.25 0 01-.25.25h-1.5a.25.25 0 01-.25-.25V7.88a.25.25 0 01.25-.25h1.5zm0-3.13a.25.25 0 01.25.25v1.125a.25.25 0 01-.25.25h-1.5a.25.25 0 01-.25-.25V4.75a.25.25 0 01.25-.25h1.5zm11.5 1.625a.25.25 0 01-.25-.25V4.75a.25.25 0 01.25-.25h1.5a.25.25 0 01.25.25v1.125a.25.25 0 01-.25.25h-1.5zm-9 3.125a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z"></path>
+</svg>
+                    </div>
+                    <h6 class=" font-semibold leading-5">
+                      Episodes
+                    </h6>
+                    <p class="text-sm text-gray-900">
+                      {{episodesDetail}}
+                    </p>
+                  </div>
+                </div>
+                <p class=" mt-2 text-base text-gray-700 md:text-lg">
+                  {{synopsisDetail}}
+                </p>
+              </div>
+
+
+            </div>
+
+          </div>
         </div>
       </div>
-      <div class="grid gap-5 row-gap-5 sm:grid-cols-2">
-        <div class="max-w-md">
-          <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-            <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-              <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-            </svg>
-          </div>
-          <h6 class="mb-2 font-semibold leading-5">The deep ocean</h6>
-          <p class="text-sm text-gray-700">
-            A flower in my garden, a mystery in my panties. Heart attack never stopped old Big Bear. I didn't even know we were calling him Big Bear.
-          </p>
+      <div class=" w-2/3 pt-16 sm:grid-cols-2">
+          <article v-for="data in animeSearchResult" :key="data.id"
+            class=" w-full rounded-xl border-2 border-gray-100 bg-white">
+            <a  @click.prevent="titleDetail=data.title,synopsisDetail=data.synopsis,imageDetail=data.image,episodesDetail=data.episodes" href="">
+<div @click.prevent="scrollToTop">
+
+  <div class="flex items-start p-6">
+    <a href="#" class="block shrink-0">
+      <img alt="poster" :src="data.thumb" class="h-32 w-20 rounded-lg object-cover" />
+    </a>
+    <div class="ml-4">
+      <h3 class="font-medium sm:text-lg">
+        <a href="#" class="hover:underline">
+          {{ data.title }}
+        </a>
+      </h3>
+
+      <p class="text-sm text-gray-700 line-clamp-2">
+        {{ data.synopsis }}
+      </p>
+
+      <div class="mt-2 sm:flex sm:items-center sm:gap-2">
+        <div class="flex items-center text-gray-500">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+            stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+          </svg>
+          <p class="ml-1 text-xs">{{ data.episodes }} episodes</p>
         </div>
-        <div class="max-w-md">
-          <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-            <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-              <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-            </svg>
-          </div>
-          <h6 class="mb-2 font-semibold leading-5">When has justice</h6>
-          <p class="text-sm text-gray-700">
-            Rough pomfret lemon shark plownose chimaera southern sandfish kokanee northern sea robin Antarctic cod. Yellow-and-black triplefin.
-          </p>
-        </div>
-        <div class="max-w-md">
-          <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-            <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-              <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-            </svg>
-          </div>
-          <h6 class="mb-2 font-semibold leading-5">Organically grow</h6>
-          <p class="text-sm text-gray-700">
-            A slice of heaven. O for awesome, this chocka full cuzzie is as rip-off as a cracker. Meanwhile, in behind the bicycle shed, Hercules.
-          </p>
-        </div>
-        <div class="max-w-md">
-          <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-            <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-              <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-            </svg>
-          </div>
-          <h6 class="mb-2 font-semibold leading-5">A slice of heaven</h6>
-          <p class="text-sm text-gray-700">
-            Disrupt inspire and think tank, social entrepreneur but preliminary thinking think tank compelling. Inspiring, invest synergy capacity.
-          </p>
-        </div>
+
+        <span class="hidden sm:block" aria-hidden="true">&middot;</span>
+
+        <p class="hidden sm:block sm:text-xs sm:text-gray-500">
+          Genre
+          <span v-for="genre in data.genres" href="#" class="font-medium underline hover:text-gray-700">
+            {{ genre }}
+          </span>
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="flex justify-end">
+    <strong
+      class="-mr-[2px] -mb-[2px] inline-flex items-center gap-1 rounded-tl-xl rounded-br-xl bg-green-600 py-1.5 px-3 text-white">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+        stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+      </svg>
+
+      <span class="text-[10px] font-medium sm:text-xs">{{ data.status }}</span>
+    </strong>
+  </div>
+</div>
+            </a>
+          </article>
+
+
       </div>
     </div>
   </div>
