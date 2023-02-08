db95d493feb28c37fe2dd0ef417027fcc9adef1e
0wx <xvnyan@gmail.com>
2023-02-08 10:33:10 +0700

    feat: add notfication

---
+++ client/src/components/molecules/Notification.vue
@@ -0,0 +1,55 @@
+<template>
+  <template>
+    <NotificationGroup group="error">
+      <div
+        class="fixed inset-0 flex items-start justify-end p-6 px-4 py-12 pointer-events-none z-[1000]"
+      >
+        <div class="w-full max-w-xs">
+          <Notification
+            v-slot="{ notifications }"
+            enter="transform ease-out duration-300 transition"
+            enter-from="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-4"
+            enter-to="translate-y-0 opacity-100 sm:translate-x-0"
+            leave="transition ease-in duration-500"
+            leave-from="opacity-100"
+            leave-to="opacity-0"
+            move="transition duration-500"
+            move-delay="delay-300"
+          >
+            <div
+              class="flex w-full max-w-sm mx-auto mt-4 overflow-hidden bg-base-300 rounded-lg shadow-md"
+              v-for="notification in notifications"
+              :key="notification.id"
+            >
+              <div class="flex items-center justify-center w-12 bg-[#E50914]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="white"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                  />
+                </svg>
+              </div>
+
+              <div class="px-4 py-2 -mx-3">
+                <div class="mx-3">
+                  <span class="font-semibold text-[#E50914]">{{
+                    notification.title
+                  }}</span>
+                  <p class="text-sm text-white">{{ notification.text }}</p>
+                </div>
+              </div>
+            </div>
+          </Notification>
+        </div>
+      </div>
+    </NotificationGroup>
+  </template>
+</template>
