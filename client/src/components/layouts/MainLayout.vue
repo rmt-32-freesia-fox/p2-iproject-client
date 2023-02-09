5cd24f53f3bf4446995dde16cd2270f6107b969a
0wx <xvnyan@gmail.com>
2023-02-09 10:48:50 +0700

    feat: add bg meh lali

--- client/src/components/layouts/MainLayout.vue
+++ client/src/components/layouts/MainLayout.vue
@@ -1,4 +1,6 @@
 <script>
+import { mapState } from 'pinia'
+import { useProfileStore } from '../../stores/profile'
 import GithubIcon from '../atoms/GithubIcon.vue'
 import Navbar from '../organisms/Navbar.vue'
 export default {
@@ -6,12 +8,20 @@ export default {
     Navbar,
     GithubIcon,
   },
+  computed: mapState(useProfileStore, ['profile', 'loading']),
 }
 </script>
 
 <template>
   <Navbar />
-  <div class="flex flex-col min-h-screen">
+  <div
+    class="flex flex-col min-h-screen"
+    :style="
+      profile && !loading && $route.name === 'profile' && profile.background
+        ? `background:linear-gradient(to bottom, rgb(0,0,0,0.9), rgb(0,0,0,0.7)), url(${profile.background}); background-size: cover; background-position: center;`
+        : ''
+    "
+  >
     <div class="px-5 md:px-10 pb-10 pt-24 flex-grow">
       <slot />
     </div>
