f5c5206cf6fcaab7572b33c64a629be7de82430a
mmacnesa <mmacnesa@gmail.com>
2023-02-13 19:35:05 +0700

    feat: basic layouting done. many part needs improvements

--- client/src/components/MusicCard.vue
+++ client/src/components/MusicCard.vue
@@ -1,22 +1,22 @@
 <script>
 
-  export default {
-    props: ['data'],
-    methods: {
-      msToTimeFormat(milliseconds) {
-        var minutes = Math.floor(milliseconds / (1000 * 60));
-        var seconds = Math.floor((milliseconds % (1000 * 60)) / 1000);
-        return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
-      },
-    }
+export default {
+  props: ['data'],
+  methods: {
+    msToTimeFormat(milliseconds) {
+      var minutes = Math.floor(milliseconds / (1000 * 60));
+      var seconds = Math.floor((milliseconds % (1000 * 60)) / 1000);
+      return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+    },
   }
+}
 
 </script>
 
 
 <template>
 
-  <div 
+  <div
     class=" mt-3 w-80 h-20 shadow-xl border border-green-800 rounded-3xl grid grid-flow-col justify-evenly items-center ">
     <div class="w-12 h-12 rounded-sm border border-black overflow-hidden ">
       <img :src="data.album.images[1].url" width="100" height="100" alt="">
@@ -28,6 +28,7 @@
     <div class="">
       <p>{{ msToTimeFormat(data.duration_ms) }}</p>
     </div>
+
 
   </div>
 
